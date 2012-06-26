0e815dd04edd0a52cd7f12b95a5991aaaf39d105
unknown <ahatch1490@Francisco.(none)>
2012-06-26 16:02:38 -0700

    Started adding api property mappings

---
+++ DotNet/Common/PayTrace.Integration/SecureCheckout/APIMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PayTrace.Integration.SecureCheckout
+{
+    
+    internal class APIMapper
+    {
+        public readonly Dictionary<string, string> PropertyToAPIMapings;
+
+        public APIMapper()
+        {
+            PropertyToAPIMapings = GetMappings();
+        }
+
+        private Dictionary<string, string> GetMappings()
+        {
+            throw new NotImplementedException();
+            //PropertyToAPIMapings = new Dictionary<string, string>();
+            //PropertyToAPIMapings.Add()
+        }
+        
+
+    }
+
+    
+
+      
+}
