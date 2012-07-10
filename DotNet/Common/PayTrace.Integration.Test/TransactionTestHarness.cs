ea7c215bc6b2e0539a28f6b24f9ccd633baf05dc
Anthony Hatch <ahatch1490@gmail.com>
2012-07-10 14:47:26 -0700

    added new test harness for transactions and moved credit card test here

---
+++ DotNet/Common/PayTrace.Integration.Test/TransactionTestHarness.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PayTrace.Integration;
+using PayTrace.Integration.API;
+
+namespace PayTrace.Integration.Test
+{
+    [TestClass]
+    public class TransactionTestHarness
+    {
+        [TestMethod]
+        public void CreditCard_should_pass_validation_tests()
+        {
+            AddressInfo billing_address = new AddressInfo("2134 happy lane", city: "Seattle", region: "WA", postalcode: "98136", country: "United States");
+            DateTime? experation_date = null;
+            experation_date = new DateTime(2015, 1, 1);
+            CreditCard cc = new CreditCard(cc: "4111111111111111", amount: "1.00", billing_address: billing_address, experation_date: experation_date);
+            cc.Validate();
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CreditCardValidationException))]
+        public void CeditCard_should_throw_exception_when_validation_is_called_and_the_date_is_not_set()
+        {
+            AddressInfo billing_address = new AddressInfo("1234 happy lane", city: "Seattle", region: "WA", postalcode: "98136", country: "United States");
+            CreditCard cc = new CreditCard(cc: "4111111111111111", amount: "1.00", billing_address: billing_address);
+
+            cc.Validate();
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CreditCardValidationException))]
+        public void CreditCard_should_throw_exception_when_validation_is_called_with_bogus_credit_card_number()
+        {
+            AddressInfo billing_address = new AddressInfo("1234 happy lane", city: "Seattle", region: "WA", postalcode: "98136", country: "United States");
+            DateTime? experation_date = null;
+            experation_date = new DateTime(2015, 1, 1);
+            CreditCard cc = new CreditCard(cc: "4311111111111111", amount: "1.00", billing_address: billing_address, experation_date: experation_date);
+
+            cc.Validate();
+        }
+    }
+}
---
+++ DotNet/Common/PayTrace.Integration/TranasctionRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PayTrace.Integration.API;
+
+namespace PayTrace.Integration
+{
+    public class TranasctionRequest : Request
+    {
+        public void AddCreditCardInfo(CreditCard cc)
+        {
+            AddressInfo billing_address = cc.BillingAddress;
+
+            APIAttributeValues.Add(Keys.CC, cc.Number);
+            APIAttributeValues.Add(Keys.EXPMNTH, cc.ExperationDate.Value.Month.ToString());
+            APIAttributeValues.Add(Keys.EXPYR, cc.ExperationDate.Value.Year.ToString());
+            APIAttributeValues.Add(Keys.CSC, cc.CSC);
+            APIAttributeValues.Add(Keys.SADDRESS, billing_address.Street);
+            APIAttributeValues.Add(Keys.SADDRESS2, billing_address.Street2);
+            APIAttributeValues.Add(Keys.SCITY, billing_address.City);
+            APIAttributeValues.Add(Keys.SSTATE, billing_address.Region);
+            APIAttributeValues.Add(Keys.SZIP, billing_address.PostalCode);
+            APIAttributeValues.Add(Keys.SCOUNTY, billing_address.County);
+            APIAttributeValues.Add(Keys.SCOUNTRY, billing_address.Country);
+        }
+    }
+}
