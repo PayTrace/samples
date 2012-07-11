b540f2dce6fc3ce691636c31eea0e21cfe2d59be
ahatch1490@gmail.com <ahatch1490@Francisco.(none)>
2012-07-11 16:19:54 -0700

    fix to transaction object

---
+++ DotNet/Common/PayTrace.Integration/TransactionResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PayTrace.Integration.API;
+
+namespace PayTrace.Integration
+{
+    public class TransactionResponse
+    {
+        public string TransactionID { get; set; }
+        public string AppCode { get; set; }
+        public string Message {get;set;}
+        
+        public TransactionResponse(Response response)
+        {
+            Response
+        }
+    }
+}
