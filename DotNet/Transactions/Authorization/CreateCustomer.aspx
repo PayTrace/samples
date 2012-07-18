91d17d95631c02f0ac43a2994342e28b23c139ff
ahatch1490@gmail.com <ahatch1490@gmail.com>
2012-07-18 11:29:39 -0700

    added ui example code for creating a customer

---
+++ DotNet/Transactions/Authorization/CreateCustomer.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CreateCustomer.aspx.cs" Inherits="Authorization.CreateCustomer" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>
+        Create Customer
+    </h2>
+        <div style="float:left;width:42%;" >
+            <fieldset>
+            <legend>Customer Form</legend>
+                <p>
+                    <asp:Label ID="CustomerNameLabel" runat="server" AssociatedControlID="tbCustomerName">Customer Name:</asp:Label><br />
+                    <asp:TextBox runat="server" ID="tbCustomerName" Text="John Doe" CssClass="textEntry" />
+                </p>
+                <p>
+                    <asp:Label ID="EmailLabel" runat="server" AssociatedControlID="tbEmail">Email:</asp:Label><br />
+                    <asp:TextBox runat="server" ID="tbEmail"  Text="test@example.com" CssClass="textEntry" />
+                </p>
+                <p>
+                    <asp:Label ID="PhoneLabel" runat="server" AssociatedControlID="tbPhone">Phone:</asp:Label><br />
+                    <asp:TextBox runat="server" ID="tbPhone" Text="123-123-1234"  CssClass="textEntry"/>
+                </p>
+                <p>
+                    <asp:Label ID="FaxLabel" runat="server" AssociatedControlID="tbFax">Fax:</asp:Label><br />
+                    <asp:TextBox runat="server" ID="tbFax" CssClass="textEntry"  />
+                </p>
+            </fieldset>
+            </div>
+            <div style="float:right;width:42%;"> 
+                <fieldset >
+                <legend>Customer Login Info</legend>
+                    <p>
+                        <label>Customer User Name</label><br />
+                     <asp:TextBox runat="server" ID="TextUserName" CssClass="textEntry" />
+                    </p>
+                    <p>
+                        <label>Customer Password</label><br />
+                        <asp:TextBox TextMode="Password" runat="server" ID="txtPwd"  CssClass="passwordEntry" />
+                    </p>
+                    </fieldset>       
+                <fieldset >
+                <legend>Customer Account Info</legend>
+                    <p>
+                        <label>Credit Card Number:</label><br />
+                     <asp:TextBox runat="server" ID="TextBox1" CssClass="textEntry" Text="4111111111111111" />
+                    </p>
+                    <p>
+                        <label> Experation Month:</label>
+                        <asp:TextBox TextMode="SingleLine" runat="server" ID="tbExperMonth"  CssClass="" MaxLength="2" Text="1" Width="10px"  />
+                         <label> Experation Year:</label>
+                        <asp:TextBox TextMode="SingleLine" runat="server" ID="tbYear"  CssClass="" MaxLength="4" Text="2015" Width="30px"   />
+                    </p>
+                    </fieldset>  
+                </div>       
+            
+</asp:Content>
---
+++ DotNet/Transactions/Authorization/Scripts/Site.js
@@ -0,0 +1,72 @@
+﻿
+function GetCountries() {
+    var countries = new Array("Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo, Democratic Republic", "Congo, Republic of the", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Greenland", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Mongolia", "Morocco", "Monaco", "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Samoa", "San Marino", " Sao Tome", "Saudi Arabia", "Senegal", "Serbia and Montenegro", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe");
+    return countries;
+}
+
+function GetStates() {
+    var usStates = [
+    { name: 'ALABAMA', abbreviation: 'AL' },
+    { name: 'ALASKA', abbreviation: 'AK' },
+    { name: 'AMERICAN SAMOA', abbreviation: 'AS' },
+    { name: 'ARIZONA', abbreviation: 'AZ' },
+    { name: 'ARKANSAS', abbreviation: 'AR' },
+    { name: 'CALIFORNIA', abbreviation: 'CA' },
+    { name: 'COLORADO', abbreviation: 'CO' },
+    { name: 'CONNECTICUT', abbreviation: 'CT' },
+    { name: 'DELAWARE', abbreviation: 'DE' },
+    { name: 'DISTRICT OF COLUMBIA', abbreviation: 'DC' },
+    { name: 'FEDERATED STATES OF MICRONESIA', abbreviation: 'FM' },
+    { name: 'FLORIDA', abbreviation: 'FL' },
+    { name: 'GEORGIA', abbreviation: 'GA' },
+    { name: 'GUAM', abbreviation: 'GU' },
+    { name: 'HAWAII', abbreviation: 'HI' },
+    { name: 'IDAHO', abbreviation: 'ID' },
+    { name: 'ILLINOIS', abbreviation: 'IL' },
+    { name: 'INDIANA', abbreviation: 'IN' },
+    { name: 'IOWA', abbreviation: 'IA' },
+    { name: 'KANSAS', abbreviation: 'KS' },
+    { name: 'KENTUCKY', abbreviation: 'KY' },
+    { name: 'LOUISIANA', abbreviation: 'LA' },
+    { name: 'MAINE', abbreviation: 'ME' },
+    { name: 'MARSHALL ISLANDS', abbreviation: 'MH' },
+    { name: 'MARYLAND', abbreviation: 'MD' },
+    { name: 'MASSACHUSETTS', abbreviation: 'MA' },
+    { name: 'MICHIGAN', abbreviation: 'MI' },
+    { name: 'MINNESOTA', abbreviation: 'MN' },
+    { name: 'MISSISSIPPI', abbreviation: 'MS' },
+    { name: 'MISSOURI', abbreviation: 'MO' },
+    { name: 'MONTANA', abbreviation: 'MT' },
+    { name: 'NEBRASKA', abbreviation: 'NE' },
+    { name: 'NEVADA', abbreviation: 'NV' },
+    { name: 'NEW HAMPSHIRE', abbreviation: 'NH' },
+    { name: 'NEW JERSEY', abbreviation: 'NJ' },
+    { name: 'NEW MEXICO', abbreviation: 'NM' },
+    { name: 'NEW YORK', abbreviation: 'NY' },
+    { name: 'NORTH CAROLINA', abbreviation: 'NC' },
+    { name: 'NORTH DAKOTA', abbreviation: 'ND' },
+    { name: 'NORTHERN MARIANA ISLANDS', abbreviation: 'MP' },
+    { name: 'OHIO', abbreviation: 'OH' },
+    { name: 'OKLAHOMA', abbreviation: 'OK' },
+    { name: 'OREGON', abbreviation: 'OR' },
+    { name: 'PALAU', abbreviation: 'PW' },
+    { name: 'PENNSYLVANIA', abbreviation: 'PA' },
+    { name: 'PUERTO RICO', abbreviation: 'PR' },
+    { name: 'RHODE ISLAND', abbreviation: 'RI' },
+    { name: 'SOUTH CAROLINA', abbreviation: 'SC' },
+    { name: 'SOUTH DAKOTA', abbreviation: 'SD' },
+    { name: 'TENNESSEE', abbreviation: 'TN' },
+    { name: 'TEXAS', abbreviation: 'TX' },
+    { name: 'UTAH', abbreviation: 'UT' },
+    { name: 'VERMONT', abbreviation: 'VT' },
+    { name: 'VIRGIN ISLANDS', abbreviation: 'VI' },
+    { name: 'VIRGINIA', abbreviation: 'VA' },
+    { name: 'WASHINGTON', abbreviation: 'WA' },
+    { name: 'WEST VIRGINIA', abbreviation: 'WV' },
+    { name: 'WISCONSIN', abbreviation: 'WI' },
+    { name: 'WYOMING', abbreviation: 'WY' }
+];
+
+    return usStates;
+}
+
